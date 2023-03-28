ca1d1a53394e9be55c37574ea3fa1eac8e29d3ae
Blessonpx <blessonpxavier@gmail.com>
2023-03-28 14:20:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("myButton");
+var output = document.getElementById("output");
+
+button.addEventListener("click", function() {
+	output.innerHTML = "Hello, World!";
+});
